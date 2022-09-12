332be5197a991b81cd224115160a5024d5f41f57
Heloa Vitoria Silva <heloavfsilva@gmail.com>
2022-09-13 00:49:06 +0200

    add Docker config

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +11,6 @@ def pagina_inicial():
     return "Laboratório DevOps - FIAP 8ASO"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
+ 
